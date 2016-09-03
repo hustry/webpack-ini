142b752cee7cf4a11c4cc6d62ecad7a0122a32d7
hustry <renyong357@qq.com>
2016-09-03 15:12:01 +0800

    add the component

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+
+
+import React from "react";
+
+export default class App extends React.Component{
+
+	render(){
+		return (
+			<h1>This is Webpack and React Startup!</h1>
+			);
+	}
+
+}
--- app/index.js
+++ app/index.js
@@ -1,15 +1,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App"
 
-class App extends React.Component{
-
-	render(){
-		return (
-			<h1>This is Webpack and React Startup!</h1>
-			);
-	}
-
-}
 
 ReactDOM.render(<App />,document.getElementById('content'));
